47715740309612009c7507cb9c8aa6dc5f774555
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-22 17:50:47 -0400

    Word search horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,26 +1,31 @@
+// check the array horizontally.
 const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word))
         {
         return true;
-        }
-    }
+        };
+    };
+// making anew array by make the colums to be rows.
     const newArray = [];
     for (let i=0; i < letters.length; i++){
         newArray[i]=[];
         for( let j=0; j< letters[i].length; j++){
            newArray[i][j]= letters[j][i];
-        }
-    }
+        };
+    };
+// check the new array horizontally to be as checking our main array vertically.
     const verticalJoin = newArray.map(ls => ls.join(''))
     for (let ele of verticalJoin ){
         if(ele.includes(word)){
     return true;
-        }
-    }
+        };
+    };
     return false;
-}
+};
+
+
 module.exports = wordSearch
 
 
