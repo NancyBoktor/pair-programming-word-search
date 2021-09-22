898041fa81978bceebad49da650ee044e0eb1083
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-22 17:47:10 -0400

    Word search horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word))
+        {
+        return true;
+        }
     }
+    const newArray = [];
+    for (let i=0; i < letters.length; i++){
+        newArray[i]=[];
+        for( let j=0; j< letters[i].length; j++){
+           newArray[i][j]= letters[j][i];
+        }
+    }
+    const verticalJoin = newArray.map(ls => ls.join(''))
+    for (let ele of verticalJoin ){
+        if(ele.includes(word)){
+    return true;
+        }
+    }
+    return false;
 }
-
 module.exports = wordSearch
+
+
+
+
+
+
+
+
+
+
+
+
+
